8cf58ae7f112390535f5fd1f20608c5e94f02f5d
Gant <GantMan@gmail.com>
2013-07-24 10:40:13 -0500

    CODE ALL THE THINGS!

---
+++ app/root_controller.rb
@@ -0,0 +1,61 @@
+class RootController < UIViewController
+  def viewDidLoad
+    view.backgroundColor = UIColor.whiteColor
+
+    # Social Framework Action Sheet
+    @buttons = ['Camera Roll']
+    @social_options = {"Twitter" => SLServiceTypeTwitter, "Facebook" => SLServiceTypeFacebook}
+    @social_options.each do |social|
+      # dup the element because it will try to pass reference to frozen asset
+      @buttons << social[0].dup if SLComposeViewController.isAvailableForServiceType(social[1])
+    end
+
+    sheet = UIActionSheet.alloc.initWithTitle(nil,
+      delegate:self,
+      cancelButtonTitle:nil,
+      destructiveButtonTitle:nil,
+      otherButtonTitles:nil)
+    @buttons.each do |btn|
+      sheet.addButtonWithTitle(btn)
+    end
+    sheet.cancelButtonIndex = sheet.addButtonWithTitle('Cancel')
+    sheet.showInView(view)
+
+    # # THIS IS HOW TO DO IT REAL QUICK WITH SUGARCUBE
+    # # https://github.com/rubymotion/sugarcube
+    # @buttons = ['Cancel', nil, 'Camera Roll']
+    # UIActionSheet.alert(nil, buttons: @buttons,
+    #   cancel: proc {p "Cancel - doing nothing is ok"  },
+    #   destructive: nil,
+    #   success: proc do |pressed|
+    #     if @social_options.include? pressed
+    #       social_media_post(@social_options[pressed])
+    #     elsif pressed == 'Camera Roll'
+    #       p "Save to camera roll"
+    #     end
+    #   end
+    # )
+  end
+
+  def actionSheet(sheet, didDismissWithButtonIndex:buttonIndex)
+    button = @buttons[buttonIndex]
+    social_media_post(@social_options[button])
+  end
+
+  def social_media_post service
+    p "load social media sheet for #{service}"
+    social_controller = SLComposeViewController.composeViewControllerForServiceType(service)
+    social_controller.setInitialText("@iconoclastlabs ROX!")
+    #social_controller.addImage(@some_picture)
+    social_controller.addURL(NSURL.URLWithString("http://www.iconoclastlabs.com"))
+    social_controller.completionHandler = lambda do |result|
+      case result
+        when SLComposeViewControllerResultDone
+          p "Social post successful"
+        when SLComposeViewControllerResultCancelled
+          p "Social post cancelled"
+      end
+    end    
+    presentModalViewController(social_controller, animated:true)   
+  end
+end
